f12a6cbfee846ccd291cb071c06b88107b52aeb2
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-09-27 18:11:10 +0100

    fix linter errors

--- person.rb
+++ person.rb
@@ -1,20 +1,21 @@
 class Person
   attr_accessor :name, :age, :parent_permission
   attr_reader :id
-  def initialize(age, name = "unknown", parent_permission: true)
+
+  def initialize(age, name = 'unknown', parent_permission: true)
     @id = Time.new.to_i
     @age = age
     @name = name
     @parent_permission = parent_permission
   end
 
-  def is_of_age
+  def of_age
     @age >= 18
   end
-  
-  private: is_of_age
+
+  private :is_of_age
 
   def can_use_service
-    is_of_age || @parent_permission == true
+    of_age || @parent_permission == true
   end
 end
