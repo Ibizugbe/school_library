2b72e85a9636668f2b81890fe48636b34f53dda4
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-09-30 10:17:28 +0100

    add method for rental list

--- app.rb
+++ app.rb
@@ -116,4 +116,15 @@ class App
     end
   end
 
+  def rental_list
+    puts 'Enter person id'
+    id = gets.chomp.to_i
+    @rentals.each do |rental|
+      if rental.person.id.to_i == id.to_i
+        puts "Date: #{rental.date}, Book: '#{rental.book.title}' by #{rental.book.author}"
+
+      end
+    end
+  end
+
 end
