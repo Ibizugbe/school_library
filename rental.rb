387ca9b040e4aaf3f02c974d458f178b51695e0c
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-09-30 05:29:07 +0100

    vreate instance variables in rental class

--- rental.rb
+++ rental.rb
@@ -2,5 +2,11 @@ require './person'
 require './book'
 
 class Rental
-  
+  def initialize(date, book, person)
+    @date = date
+    @book = book
+    book.rentals << self
+    @person = person
+    person.rentals << self
+  end
 end
