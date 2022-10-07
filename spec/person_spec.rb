25cad7cea85e17c169f088ffd795de0cf735aa10
Samuel Ackon <ackons433@gmail.com>
2022-10-07 10:46:08 +0000

    test person spec

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../person'
 describe Person do
-  let(:person) { Person.new(23, 'Smith') }
+  let(:person) { Person.new(67, 'Doana') }
 
   describe '#new' do
     it 'creates a new person' do
@@ -8,11 +8,11 @@ describe Person do
     end
 
     it 'has a name' do
-      expect(person.name).to eq('Smith')
+      expect(person.name).to eq('Doana')
     end
 
     it 'has an age' do
-      expect(person.age).to eq(23)
+      expect(person.age).to eq(67)
     end
   end
 end
