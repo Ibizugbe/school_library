12878dc97392b6c50440352982893eff8b420b41
Samuel Ackon <ackons433@gmail.com>
2022-10-07 10:43:47 +0000

    test capitalize decorator

--- spec/capitalize_decorator_spec.rb
+++ spec/capitalize_decorator_spec.rb
@@ -2,12 +2,12 @@ require_relative '../decorator'
 require_relative '../person'
 
 describe CapitalizeDecorator do
-  person = Person.new(23, 'Smithjohn@gmail.com')
+  person = Person.new(45, 'johndoe@gmail.com')
   capitalize = CapitalizeDecorator.new(person)
 
   describe '#correct_name' do
     it 'capitalizes the email' do
-      expect(capitalize.correct_name).to eq('Smithjohn@gmail.com')
+      expect(capitalize.correct_name).to eq('johndoe@gmail.com')
     end
   end
 end
